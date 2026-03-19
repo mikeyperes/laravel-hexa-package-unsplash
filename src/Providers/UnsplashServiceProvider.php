769bbb891f743107e119d76970d902396cf8173b
mikeyperes <michael@mike-ro-tech.com>
2026-03-18 21:01:11 -0400

    Add config with version, mergeConfigFrom, update description — v1.0.0

--- src/Providers/UnsplashServiceProvider.php
+++ src/Providers/UnsplashServiceProvider.php
@@ -9,6 +9,7 @@ class UnsplashServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        ->mergeConfigFrom(__DIR__ . '/../../config/unsplash.php', 'unsplash');
         $this->app->singleton(UnsplashService::class);
     }
 
