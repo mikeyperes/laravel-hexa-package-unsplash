68c79a8c701d4b59b676c6d4231e99b715b14dac
mikeyperes <mikeyperes@users.noreply.github.com>
2026-04-27 03:29:41 +0000

    feat(unsplash): register package metadata

--- src/Providers/UnsplashServiceProvider.php
+++ src/Providers/UnsplashServiceProvider.php
@@ -42,6 +42,13 @@ class UnsplashServiceProvider extends ServiceProvider
             // HWS-SIDEBAR-MENU-3L-END
 
             $registry->registerSidebarLink('unsplash.index', 'Unsplash', 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z', 'Sandbox', 'unsplash', 86);
+            $registry->registerPackage('unsplash', 'hexawebsystems/laravel-hexa-package-unsplash', [
+                'title' => 'Unsplash',
+                'description' => 'Stock image search and import tooling powered by the Unsplash API.',
+                'icon' => 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z',
+                'color' => 'amber',
+                'settingsRoute' => 'unsplash.index',
+            ]);
         }
     }
 }
